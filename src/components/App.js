0374f6dd59e44e097edb655ffd7e6dfc5ff37aa9
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-17 09:53:31 -0700

    added props notes

--- src/components/App.js
+++ src/components/App.js
@@ -15,5 +15,45 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
+//////////////////////////NOTES ON PROPS PATH//////////////////////////
+//------🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥----------------->
+// from App.js ->
+// function Home({ city, name, color }) {
+//   return (
+//     <div id="home">
+//       <h1 style={{ color: color }}>
+//         {name} is a Web Developer from {city}
+//       </h1>
+//     </div>
+//   );
+// }
+// export default Home;
+//------🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥----------------->
+//------🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉----------------->
+// import Links from "./Links";
+// function About({ bio, links }) {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       {bio && <p>{bio}</p>}
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       <Links github={links.github} linkedin={links.linkedin} />
+//     </div>
+//   );
+// }
+// export default About;
+//------🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉🐉----------------->
+//------🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨----------------->
+// const Links = ({ github, linkedin }) => {
+//   return (
+//     <div>
+//       <h3>Links</h3>
+//       <a href={github}>{github}</a>
+//       <a href={linkedin}>{linkedin}</a>
+//     </div>
+//   );
+// };
+// export default Links;
+//------🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨----------------->
